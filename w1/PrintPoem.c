54cf4483e13a6e793019c8ca5955a76c41e67f14
caroleatierney@gmail.com <59835292+caroleatierney@users.noreply.github.com>
2020-11-29 22:02:40 -0500

    rest of w1 c code

---
+++ w1/PrintPoem.c
@@ -0,0 +1,19 @@
+/* Print Poem */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\nNothing Gold Can Stay\n");
+    printf("Robert Frost - 1872-1963\n\n");
+    printf("Nature's first green is gold,\n");
+    printf("Her hardest hue to hold.\n");
+    printf("Her early leaf's a flower;\n");
+    printf("But only so an hour.\n");
+    printf("Then leaf subsides to leaf.\n");
+    printf("So Eden sank to grief,\n");
+    printf("So dawn goes down today.\n");
+    printf("Nothing Gold Can Stay.\n\n");
+
+    return 0;
+}
